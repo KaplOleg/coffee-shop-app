c215a9e0be51064ed6b832e5d5e1b37d3e6b5331
OlegKapl <kaplun.olegg@gmail.com>
2022-10-14 10:18:18 +0300

    add general component beans-logo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import BeansLogo from './general/beans-logo/beans-logo';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello</h1>
+    <>
+      <BeansLogo/>
+    </>
 );
 
