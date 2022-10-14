88f4dcf80bf35f77ce28f9657fa27182feb9b514
OlegKapl <kaplun.olegg@gmail.com>
2022-10-14 10:34:41 +0300

    add genereal component Header

---
+++ src/general/header/header.js
@@ -0,0 +1,20 @@
+import logo from '../../icons/coffee-beans-logo.png'
+
+import './header.sass'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header_logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <ul className="menu">
+                <li className="menu_item"><a href="#home" className="menu_link">Coffee house</a></li>
+                <li className="menu_item"><a href="#first" className="menu_link">Our coffee</a></li>
+                <li className="menu_item"><a href="#second" className="menu_link">For your pleasure</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BeansLogo from './general/beans-logo/beans-logo';
+import Header from './general/header/header';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-      <BeansLogo/>
+      <Header/>
     </>
 );
 
